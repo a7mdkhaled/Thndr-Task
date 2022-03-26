36c840f128b852625734d3dcc155a4ce118ad758
Ahmed <a7md.khaled@gmail.com>
2022-03-26 23:26:37 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { createOvermind } from 'overmind';
+import { Provider } from 'overmind-react';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { config } from './App/overmind';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const overmind = createOvermind(config);
 
   if (!isLoadingComplete) {
     return null;
   }
   return (
     <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
+      <Provider value={overmind}>
+        <Navigation colorScheme={colorScheme} />
+      </Provider>
       <StatusBar />
     </SafeAreaProvider>
   );
