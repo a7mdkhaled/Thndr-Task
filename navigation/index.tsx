19f79dc94534fdb6db66a34e332097a769073c4c
Ahmed <a7md.khaled@gmail.com>
2022-03-26 23:27:47 +0200

    [FEAT] Navigation

---
+++ navigation/index.tsx
@@ -0,0 +1,54 @@
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { ColorSchemeName } from 'react-native';
+import ExploreScreen from '../App/containers/explore/explore.container';
+import StockDetails from '../App/containers/stock-details/stock-details.container';
+import Colors from '../constants/Colors';
+import { RootStackParamList } from '../types';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Root"
+        component={ExploreScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="StockDetails"
+        component={StockDetails}
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: Colors.light.tint,
+          },
+          headerTitleStyle: {
+            color: 'white',
+          },
+          headerTintColor: '#fff',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
+  return (
+    <NavigationContainer
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+    >
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
