b681412888db8382596ff941b1f9541b8b829d42
Ahmed <a7md.khaled@gmail.com>
2022-03-28 20:06:39 +0200

    [FIX] enhancing fetching stocks

--- App/overmind/actions.js
+++ App/overmind/actions.js
@@ -1,12 +1,17 @@
 import { Alert } from 'react-native';
+import { FetchStocksMapper } from '../utils/mappers/fetch-stocks.mapper';
 import { StockDetailsMapper } from '../utils/mappers/stock-details.mapper';
 
 export const fetchStocks = async ({ state, effects }, params) => {
   state.stocks.loading = true;
   const stocks = await effects.api.fetchStocks(params);
   if (stocks.status === 200) {
+    // splitting cursor can also be implemented in a shared handler
     state.stocks.next_url = stocks.body.next_url.split('cursor=')[1];
-    state.stocks.data = stocks.body.results;
+    const MappedStocks = FetchStocksMapper.mapToUiModel({
+      stocks: stocks.body.results,
+    });
+    state.stocks.data = MappedStocks;
     state.stocks.loading = false;
   } else if (stocks.status === 429) {
     Alert.alert(stocks.body.error);
@@ -20,7 +25,13 @@ export const fetchMoreStocks = async ({ state, effects }, params) => {
   const stocks = await effects.api.fetchMoreStocks(params);
   if (stocks.status === 200) {
     state.stocks.next_url = stocks.body.next_url.split('cursor=')[1];
-    state.stocks.data = [...state.stocks.data, ...stocks.body.results];
+    const filtered = stocks.body.results.filter(
+      (item) => !stocks.body.results[item.ticker],
+    );
+    const MappedStocks = FetchStocksMapper.mapToUiModel({
+      stocks: filtered,
+    });
+    state.stocks.data = { ...state.stocks.data, ...MappedStocks };
     state.stocks.loadingMoreStocks = false;
   } else if (stocks.status === 429) {
     Alert.alert(stocks.body.error);
@@ -49,37 +60,21 @@ export const clearSearch = async ({ state }) => {
 
 // Stock Details
 
-export const fetchStockDetails = async ({ state, effects }, params) => {
-  state.tickerDetails.loading = true;
-  const stocks = await effects.api.fetchStockDetails(params);
-  const prices = await effects.api.fetchPrevClose(params);
+export const fetchStockDetails = async ({ state, effects }, ticker) => {
+  state.stockDetails.loading = true;
+  const stocks = await effects.api.fetchStockDetails(ticker);
+  const prices = await effects.api.fetchPrevClose(ticker);
   if (stocks.status === 200) {
     if (stocks && prices) {
       const Mapping = StockDetailsMapper.mapToUiModel({
-        stocks: stocks.body.results,
-        prices: prices.body.results[0],
+        stocks: stocks?.body?.results,
+        prices: prices?.body?.results?.[0],
       });
-      state.tickerDetails.data = Mapping;
-      state.tickerDetails.loading = false;
+      state.stockDetails.data = Mapping;
+      state.stockDetails.loading = false;
     }
-    // state.tickerDetails.data = stocks.body.results[0];
-    // state.tickerDetails.loading = false;
-  } else if (stocks.status === 429) {
-    Alert.alert(stocks.body.error);
-  } else {
-    Alert.alert('Something went wrong');
-  }
-};
-
-export const fetchPrevClose = async ({ state, effects }, params) => {
-  state.tickerDetails.loading = true;
-  const stocks = await effects.api.fetchPrevClose(params);
-  if (stocks.status === 200) {
-    state.tickerDetails.data = [
-      ...state.tickerDetails.data,
-      ...stocks.body.results,
-    ];
-    state.tickerDetails.loading = false;
+    state.stockDetails.data = stocks.body.results[0];
+    state.stockDetails.loading = false;
   } else if (stocks.status === 429) {
     Alert.alert(stocks.body.error);
   } else {
--- App/utils/endpoints.js
+++ App/utils/endpoints.js
@@ -1,5 +1,4 @@
 export const ApiUrl = 'https://api.polygon.io';
-
 export const routes = {
   fetchStocks: `${ApiUrl}/v3/reference/tickers`,
   prevClose: `${ApiUrl}/v2/aggs/ticker`,
